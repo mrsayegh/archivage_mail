eb1162be0bb8960d49174e0e6a4719dcfae0dbc4
Mohamad SAYEGH <mohamad.sayegh@devoteam.com>
2017-11-14 14:47:32 +0100

    Ajouts fichiers

--- index.js
+++ index.js
@@ -40,40 +40,44 @@ app.post('/webhook', function (req, res) {
 
   // the most basic response
   if (res.status == '200' && res.result.metadata.intentName === '2007-NON'){
-let http = require("https");
 
-let options = {
-"method": "POST",
-"hostname": "dev16424.service-now.com",
-"port": null,
-"path": "/api/now/table/incident",
-"headers": {
-"accept": "application/json",
-"content-type": "application/json",
-"authorization": "Basic YWRtaW46TW9oYW1hZCYyMw==",
-"cache-control": "no-cache",
-"postman-token": "2662f1ab-cc41-d4ed-f425-1cfa85287298"
-}
-};
 
-let req = http.request(options, function (res) {
-let chunks = [];
-
-res.on("data", function (chunk) {
-chunks.push(chunk);
-});
-
-res.on("end", function () {
-let body = Buffer.concat(chunks);
-console.log(body.toString());
-});
-});
-
-req.write(JSON.stringify({ caller_id: 'mohamad sayegh', short_description: 'TEST Mardi', active: 'true' }));
-req.end();
+    let http = require("https");
+    
+    let options = {
+    "method": "POST",
+    "hostname": "dev16424.service-now.com",
+    "port": null,
+    "path": "/api/now/table/incident",
+    "headers": {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "authorization": "Basic YWRtaW46TW9oYW1hZCYyMw==",
+    "cache-control": "no-cache",
+    "postman-token": "2662f1ab-cc41-d4ed-f425-1cfa85287298"
+    }
+    };
+    
+    let req = http.request(options, function (res) {
+    let chunks = [];
+    
+    res.on("data", function (chunk) {
+    chunks.push(chunk);
+    });
+    
+    res.on("end", function () {
+    let body = Buffer.concat(chunks);
+    console.log(body.toString());
+    });
+    });
+    
+    req.write(JSON.stringify({ caller_id: 'mohamad sayegh', short_description: 'TEST Mardi', active: 'true' }));
+    req.end();
 	
   }
 	  
+  
+})
   
   /*.json({
     source: 'webhook',
